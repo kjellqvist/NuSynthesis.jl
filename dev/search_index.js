f83befa758d637d91b8e08d739c21eb935323597
Documenter.jl <documenter@juliadocs.github.io>
2022-03-23 18:07:06 +0000

    build based on 962585f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NuSynthesis","category":"page"},{"location":"#NuSynthesis","page":"Home","title":"NuSynthesis","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NuSynthesis.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NuSynthesis]","category":"page"}]
+}
